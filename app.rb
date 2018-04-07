2393a9aedb6e842a3967e060050398e28fcea701
Sivan Patel <sivan.patel@googlemail.com>
2018-04-07 08:58:46 +0100

    app working fully now i think

--- app.rb
+++ app.rb
@@ -1,13 +1,22 @@
 require 'sinatra'
+require './lib/url_normalizer'
+require './lib/url_encrypter'
 require 'json'
 
 post '/' do
   content_type :json
   parameters = JSON.parse(request.body.read)
+  url = UrlNormalizer.normalized_url(parameters["url"])
+  short_url = "/#{UrlEncrypter.encrypt(url)}"
   return_params = {
-                    "short_url": "/abc123",
-                    "url": parameters["url"]
+                    "short_url": short_url,
+                    "url": url
                   }
   JSON[return_params]
 end
 
+get '/:short_url' do
+  short_url = params[:short_url]
+  url = UrlEncrypter.decrypt(short_url)
+  redirect url, 301
+end
--- lib/url_normalizer.rb
+++ lib/url_normalizer.rb
@@ -1,10 +1,11 @@
 class UrlNormalizer
+  class << self
+    def normalized_url(url)
+      has_http?(url) ? url : "http://#{url}"
+    end
 
-  def self.normalized_url(url)
-    has_http?(url) ? url : "http://#{url}"
-  end
-
-  def self.has_http?(url)
-    url[0..6] == 'http://'
+    def has_http?(url)
+      url[0..6] == 'http://'
+    end
   end
 end
